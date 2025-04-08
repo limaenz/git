c61b58401a474ad028930e8956156b92efcd530a
Enzo Lima <enzolimafranca@gmail.com>
2025-04-08 07:35:01 -0300

    feat: read a blob with  cat-file function

--- src/Program.cs
+++ src/Program.cs
@@ -1,5 +1,7 @@
 using System;
 using System.IO;
+using System.IO.Compression;
+using System.Text;
 
 if (args.Length < 1)
 {
@@ -9,15 +11,52 @@ if (args.Length < 1)
 
 string command = args[0];
 
-if (command == "init")
+switch (command)
 {
-    Directory.CreateDirectory(".git");
-    Directory.CreateDirectory(".git/objects");
-    Directory.CreateDirectory(".git/refs");
-    File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
-    Console.WriteLine("Initialized git directory");
-}
-else
-{
-    throw new ArgumentException($"Unknown command {command}");
+    case "init":
+
+        Directory.CreateDirectory(".git");
+        Directory.CreateDirectory(".git/objects");
+        Directory.CreateDirectory(".git/refs");
+        File.WriteAllText(".git/HEAD", "ref: refs/heads/main\n");
+        Console.WriteLine("Initialized git directory");
+        break;
+
+    case "cat-file":
+        {
+            if (args[1] != "-p" && args[2] is null)
+                return;
+
+            var objects = args[2];
+            string path = $".git/objects/{objects[..2]}/{objects.Substring(2, 38)}";
+
+            using FileStream compressedFileStream = File.OpenRead(path);
+            using var ds = new ZLibStream(compressedFileStream, CompressionMode.Decompress);
+            using var sr = new StreamReader(ds);
+
+            var decompressedObject = sr.ReadToEnd();
+
+            byte[] bytes = Encoding.ASCII.GetBytes(decompressedObject);
+            var chars = new List<char>();
+
+            bool findNull = false;
+            for (int i = 0; i < bytes.Length; i++)
+            {
+                char letter = ((char)bytes[i]);
+                if (letter == char.MinValue)
+                    findNull = true;
+
+                if (findNull)
+                    chars.Add(letter);
+            }
+
+            if (chars.Count != 0)
+            {
+                string content = new([.. chars]);
+                Console.WriteLine(content);
+            }
+        }
+        break;
+
+    default: throw new ArgumentException($"Unknown command {command}");
 }
